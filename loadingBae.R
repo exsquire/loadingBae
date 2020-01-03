d69e6a3c1d98d99e2113db3ec5a8ed1ad0a3f03b
Excel Que <34141973+exsquire@users.noreply.github.com>
2020-01-03 11:30:45 -0800

    Adding function call

--- loadingBae.R
+++ loadingBae.R
@@ -8,20 +8,21 @@
 path = "test/"
 key <- c("rownames", "colnames", "data")
 
-pathIn <- list()
-for(i in seq_along(key)){
-  pathIn[[key[i]]] = list.files(path = path,
-                                pattern = key[i],
-                                full.names = TRUE,
-                                ignore.case = TRUE)
-}
+loadingBae <- function(path, key){
+  pathIn <- list()
+  for(i in seq_along(key)){
+    pathIn[[key[i]]] = list.files(path = path,
+                                  pattern = key[i],
+                                  full.names = TRUE,
+                                  ignore.case = TRUE)
+  }
 
-#Check
-if(!all(sapply(pathIn, function(x) length(x) > 0))){
-  cat("Error: One or more input(s) could not be found.\n")
-  print(sapply(pathIn, function(x) length(x) > 0))
-}else{
-  cat("\nFound the following inputs: \n")
-  print(sapply(pathIn, function(x) length(x) > 0))
+  #Check
+  if(!all(sapply(pathIn, function(x) length(x) > 0))){
+    cat("Error: One or more input(s) could not be found.\n")
+    print(sapply(pathIn, function(x) length(x) > 0))
+  }else{
+    cat("\nFound the following inputs: \n")
+    print(sapply(pathIn, function(x) length(x) > 0))
+  }
 }
-
